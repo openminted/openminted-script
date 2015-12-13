a842d73a0e37e88bc0205d97647d8e568c0f6eb0
Richard Eckart de Castilho <richard.eckart@gmail.com>
2015-12-13 14:26:58 -0200

    #3 - Support unit tests - License header

--- dkpro-script-groovy/src/test/groovy/org/dkpro/script/groovy/DKProScriptBaseTest.groovy
+++ dkpro-script-groovy/src/test/groovy/org/dkpro/script/groovy/DKProScriptBaseTest.groovy
@@ -1,3 +1,20 @@
+// ****************************************************************************
+// Copyright 2015
+// Ubiquitous Knowledge Processing (UKP) Lab
+// Technische Universität Darmstadt
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ****************************************************************************
 package org.dkpro.script.groovy
 
 import static org.junit.Assert.*
@@ -11,7 +28,7 @@ import org.junit.runners.Parameterized.Parameters
 
 @RunWith(value = Parameterized.class)
 class DKProScriptBaseTest {
-    // Scan all the subdirs in src/test/resources and use them to parametrize the test
+    // Scan all the subdirs in src/test/resources and use them to parameterize the test
     @Parameters(name = "{index}: running script {0}")
     public static Iterable<Object[]> testScripts() {
         def dirs = [];
