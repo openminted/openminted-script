b251fc407e1a4ea64d3de26a542ae4b9f6b20d8c
Richard Eckart de Castilho <richard.eckart@gmail.com>
2016-08-31 19:13:36 +0200

    No issue. Some refactoring for the conversions; experiments towards calling LAPPS services.

--- openminted-script-groovy/src/test/groovy/eu/openminted/script/groovy/LappsExperiment.groovy
+++ openminted-script-groovy/src/test/groovy/eu/openminted/script/groovy/LappsExperiment.groovy
@@ -16,12 +16,23 @@
 // ****************************************************************************
 package eu.openminted.script.groovy
 
+import eu.openminted.script.groovy.internal.frameworkconversions.DKPro2Lif
+import eu.openminted.script.groovy.internal.frameworkconversions.Lif2DKPro
+
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.fit.factory.JCasFactory
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas
 import org.junit.Test
 import org.lappsgrid.client.ServiceClient
-import org.lappsgrid.discriminator.Discriminators.Uri;
-import org.lappsgrid.serialization.Data;
+import org.lappsgrid.discriminator.Discriminators.Uri
+import org.lappsgrid.serialization.Data
+import org.lappsgrid.serialization.DataContainer
 import org.lappsgrid.serialization.Serializer;
-import org.lappsgrid.serialization.lif.Container;
+import org.lappsgrid.serialization.lif.Container
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;;
 
 class LappsExperiment
 {
@@ -32,18 +43,50 @@ class LappsExperiment
 
     @Test
     public void test() {
-        ServiceClient client = new ServiceClient("${base}:${service}", username, password)
-        println client.getMetadata();
+        // Data<String> data  = new Data<>(Uri.TEXT, "This is a sentence. This is another one.");
+        
+        
+        JCas source = JCasFactory.createText("This is a sentence. This is another one.", "en");
 
         Container container = new Container();
-        container.setLanguage("en");
-        container.setText("This is a sentence. This is another one.");
+        new DKPro2Lif().convert(source, container);
+        
+        DataContainer whiteboard;
+        
+        ServiceClient tokenizer = new ServiceClient("${base}:stanford.tokenizer_2.0.0", username, password)
+        whiteboard = Serializer.parse(
+            tokenizer.execute(new Data<>(Uri.LIF, container).asJson()),
+            DataContainer);
+        println "- SPLITTER ---------------------------------------------------------------------";
+        println whiteboard.asPrettyJson();
 
-        Data<String> data  = new Data<>(Uri.LIF, container);
-        //        Data<String> data  = new Data<>(Uri.TEXT, "This is a sentence. This is another one.");
-
-        String result = client.execute(data.asJson());
-
-        println result;
+        ServiceClient splitter = new ServiceClient("${base}:stanford.splitter_2.0.0", username, password)
+        whiteboard = Serializer.parse(
+            splitter.execute(new Data<>(Uri.LIF, whiteboard.getPayload()).asJson()), 
+            DataContainer);
+        println "- SPLITTER ---------------------------------------------------------------------";
+        println whiteboard.asPrettyJson();
+        
+        ServiceClient tagger = new ServiceClient("${base}:stanford.tagger_2.0.0", username, password)
+        whiteboard = Serializer.parse(
+            tagger.execute(new Data<>(Uri.LIF, whiteboard.getPayload()).asJson()), 
+            DataContainer);
+        println "- TAGGER -----------------------------------------------------------------------";
+        println whiteboard.asPrettyJson();
+        
+        JCas target = JCasFactory.createJCas();
+        new Lif2DKPro().convert(whiteboard.getPayload(), target);
+        
+        println JCasUtil.select(target, Sentence).size();
+        println JCasUtil.select(target, Token).size();
     }
+    
+    private Data run(String aService)
+    {
+        Container container = new Container();
+        ServiceClient service = new ServiceClient("${base}:${aService}", username, password)
+        DataContainer result1 = Serializer.parse(
+            service.execute(new Data<>(Uri.LIF, container).asJson()),
+            DataContainer);
+    } 
 }
